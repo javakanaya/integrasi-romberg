024592fdd16c33b5e172f05a032f2d80748abbfe
jap <javakanaya@gmail.com>
2022-12-21 06:41:36 +0700

    add print method

--- src/romberg.py
+++ src/romberg.py
@@ -16,43 +16,49 @@ def trapEq(n, a, b):
     sum = sum + f(b)
     return (b - a) * sum / (2 * n)
 
-# INISIASI
-# batas bawah dan atas
-a = 0
-b = 4
+    # INISIASI
+def rombergIntegration(a, b, iteration):
 
-# Matriks  
-I = np.zeros((10, 10))
-
-# Jumlah Trapezoid
-i = 1
-
-I[1,1] = trapEq(1, a, b)
-
-# MATRIX I
-# (1,1)(1,2)(1,3)(1,4)...
-# (2,1)(2,2)(2,3)...
-# (3,1)(3,2)...
-# (4,1)...
-
-# PERHITUNGAN
-for _ in range(8):
-    # menambah jumlah trapezoid
-    n = 2 ** i
-
-    # perkiraan/approx. integasi berikutnya
-    # disimpan di kolom pertama baris baru
-    I[i + 1, 1] = trapEq(n, a, b)
-
-    # Integrasi Romberg
-    for k in range(2, i + 2):
-        j = 2 + i - k
-        # mendapatkan nilai untuk kolom terkanan(terbaru)-baris pertama
-        # dengan menggunakan nilai pada kolom-baris sebelumnya
-        I[j, k] = (4 ** (k - 1) * I[j + 1, k - 1] - I[j, k - 1]) / (4 ** (k - 1) - 1)
-
-    i += 1
+    # Matriks  
+    I = np.zeros((iteration + 1, iteration + 1))
 
 
-print(I)
-print("hasil akhir :", I[1, i])
+    I[1,1] = trapEq(1, a, b)
+
+    # MATRIX I
+    # (1,1)(1,2)(1,3)(1,4)...
+    # (2,1)(2,2)(2,3)...
+    # (3,1)(3,2)...
+    # (4,1)...
+    
+    # Hasil dengan ketelitian paling tinggi berada pada 
+    # posisi matriks paling kanan (1, iteration)
+
+    # PERHITUNGAN
+    for i in range(iteration):
+        # menambah jumlah trapezoid
+        n = 2 ** i
+
+        # integasi berikutnya dengan jumlah step yang lebih banyak
+        # disimpan di kolom pertama baris baru
+        I[i + 1, 1] = trapEq(n, a, b)
+
+        # Integrasi Romberg
+        for k in range(2, i + 2):
+            j = 2 + i - k
+            # mendapatkan nilai untuk kolom terkanan(terbaru)-baris pertama
+            # dengan menggunakan nilai pada kolom-baris sebelumnya
+            I[j, k] = (4 ** (k - 1) * I[j + 1, k - 1] - I[j, k - 1]) / (4 ** (k - 1) - 1)
+
+    # menampilkan hasil
+    print("Step\tStep Size\tResult")
+    for i in range(iteration):
+            step = 2 ** (i)
+            stepSize = b - a / step
+
+            print('%d\t%5f\t' % (step, stepSize), end='')
+            print(I[i + 1, 1:])
+                
+    print("hasil akhir :", I[1, iteration])
+
+rombergIntegration(0, 4, 1)
